3f8cdf629f091010483c9c8b6dc45ddeaf21ba52
Jaska <36661744+F2PN0b@users.noreply.github.com>
2022-01-28 20:27:30 +0200

    Winget-Auto mutta suomeksi.

--- Winget Auto.cpp
+++ Winget Auto.cpp
@@ -2,35 +2,50 @@
 //
 
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
-    int close = 1;
+    int close = 1, subclose = 1;    // stupid and lazy 
+    string appid = "hello";
     while (close == 1)
     {
         int input;
-            system("cls");
-            std::cout << "1. List installed software\n";
-            std::cout << "2. Upgarde all\n";
-            std::cout << "3. Install sofware\n";
-
-            cout << "Type a number & press enter "; // Type a number and press enter
-            cin >> input; // Get user input from the keyboard
+        system("cls");
+        std::cout << "1. Asenna sovellus\n";
+        std::cout << "2. Paivita kaikki\n";
+        std::cout << "3. Asennetetut\n";
+        std::cout << "0. Poistu\n";
+        cout << "Valintasi: ";
+        cin >> input; // Get user input from the keyboard
         switch (input)
         {
-            case 1:
-                system("winget list");
-                system("pause");
-                break;
-            case 2:
-                system("tree");
-                break;
-            case 0:
-                close = 0;
-                cout << "Have nice day!";
-                break;
+        case 3:
+            system("winget list");
+            system("pause");
+            break;
+        case 2:
+            system("winget upgrade --all");
+            break;
+        case 1:
+            system("cls");
+            while (subclose == 1)
+            {
+                cout << "Kirjoita sovelluksen nimi tai exit jos haluat poistua: ";
+                cin >> appid;
+                cout << appid;
+                if (appid == "exit") {
+                    subclose = 0;
+                    break;
+                }
+                system(("winget install " + appid).c_str()); // string "appid" in a sys call 
+            }
+
+        case 0:
+            close = 0;
+            cout << "Hyvää paskea";
+            break;
         }
     }
 }
-
